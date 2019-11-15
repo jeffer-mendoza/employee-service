77c4ceecf222f5b64707b151427845cd1303f08b
jefferson <mendosajefferson@gmail.com>
2019-11-14 21:45:10 -0500

    script is updated and postman collections are added to testing

--- doc/script-init.sql
+++ doc/script-init.sql
@@ -1,10 +1,13 @@
 create table employee
 (
-    id bigint not null,
-    base_salary float not null,
-    date_end datetime(6) null,
-    date_start datetime(6) null,
-    firstname varchar(255) null,
-    lastname varchar(255) null,
-    primary key ()
-);
+	id bigint not null primary key,
+	base_salary float null,
+	date_end datetime null,
+	date_start datetime null,
+	firstname varchar(255) null,
+	lastname varchar(255) null
+)
+
+INSERT INTO employee (id, base_salary, date_end, date_start, firstname, lastname)
+VALUES (1, 1000000, '2018-11-25 00:00:00', '2018-08-12 00:00:00', 'Juan c', 'Gonzalez');
+
